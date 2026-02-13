d73a0f6b3d5fca153e60b0580243e7a49fd9fa69
webndevs <webndevs@gmail.com>
2026-02-13 17:58:40 +0530

    Add PageTemplateSeeder to DatabaseSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             ContentSeeder::class,
+            PageTemplateSeeder::class,
         ]);
     }
 }
